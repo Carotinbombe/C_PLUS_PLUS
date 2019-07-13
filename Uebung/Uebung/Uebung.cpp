42d856494cbbc69f3750002843b995012c3fe85e
Robin Gerstmann <rgerstma@hs-mittweida.de>
2019-07-13 22:40:56 +0200

    Uebung01

---
+++ Uebung/Uebung/Uebung.cpp
@@ -0,0 +1,45 @@
+// Uebung.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include <ctime>
+#include <stdio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	int input;
+	int guess = rand();
+	bool incorrect = true;
+
+	cout << "-----Welcome-----" << endl;
+	while (incorrect) {
+		
+		cout << "Enter a number: ";
+		cin >> input;
+
+		if (input < guess) {
+			cout << "Wrong! Its greather than: " << input << endl;
+		}
+		else if (input > guess) {
+			cout << "Wrong! Its lower than: " << input << endl;
+		}
+		else {
+			system("cls");
+			cout << "Congratulation! You got it!" << endl;
+			incorrect = false;
+
+			
+		}
+	}
+
+	while (1) {
+		system("color 0A");
+		Sleep(500);
+		system("color 01");
+		Sleep(500);
+	}
+	return EXIT_SUCCESS;
+}
